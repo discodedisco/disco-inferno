a19105d2c363eafe6ea6ad96d170a84cce2118bb
Disco DeDisco <adamcraighamilton@gmail.com>
2025-07-09 22:17:42 -0400

    extended base.html into index.html and wheel.html; created admin area & linked custom admin styles; elaborated on included partials

--- discman/settings.py
+++ discman/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
+                'django.template.context_processors.static',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -121,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'pages/index.html')
+    
+def wheel(request):
+    return render(request, 'pages/wheel.html')
