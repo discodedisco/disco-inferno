ce080b1cd08c6e66ca63ea3edfbbf36a2bc58166
Disco DeDisco <adamcraighamilton@gmail.com>
2025-07-13 14:41:49 -0400

    sitewide styling; sample db created for /nativity/constellations.py;

---
+++ nativity/constellation.py
@@ -0,0 +1,27 @@
+planet_options = [
+    {'id': 'sun', 'symbol': '&#9737;', 'name': 'Sun'},
+    {'id': 'moon', 'symbol': '&#9789;', 'name': 'Moon'},
+    {'id': 'mercury', 'symbol': '&#9791;', 'name': 'Mercury'},
+    {'id': 'venus', 'symbol': '&#9792;', 'name': 'Venus'},
+    {'id': 'mars', 'symbol': '&#9794;', 'name': 'Mars'},
+    {'id': 'jupiter', 'symbol': '&#9795;', 'name': 'Jupiter'},
+    {'id': 'saturn', 'symbol': '&#9796;', 'name': 'Saturn'},
+    {'id': 'uranus', 'symbol': '&#9797;', 'name': 'Uranus'},
+    {'id': 'neptune', 'symbol': '&#9798;', 'name': 'Neptune'},
+    {'id': 'pluto', 'symbol': '&#9799;', 'name': 'Pluto'},
+]
+
+sign_options = [
+    {'id': 'aries', 'symbol': '&#9800;', 'name': 'Aries'},
+    {'id': 'taurus', 'symbol': '&#9801;', 'name': 'Taurus'},
+    {'id': 'gemini', 'symbol': '&#9802;', 'name': 'Gemini'},
+    {'id': 'cancer', 'symbol': '&#9803;', 'name': 'Cancer'},
+    {'id': 'leo', 'symbol': '&#9804;', 'name': 'Leo'},
+    {'id': 'virgo', 'symbol': '&#9805;', 'name': 'Virgo'},
+    {'id': 'libra', 'symbol': '&#9806;', 'name': 'Libra'},
+    {'id': 'scorpio', 'symbol': '&#9807;', 'name': 'Scorpio'},
+    {'id': 'sagittarius', 'symbol': '&#9808;', 'name': 'Sagittarius'},
+    {'id': 'capricorn', 'symbol': '&#9809;', 'name': 'Capricorn'},
+    {'id': 'aquarius', 'symbol': '&#9810;', 'name': 'Aquarius'},
+    {'id': 'pisces', 'symbol': '&#9811;', 'name': 'Pisces'},
+]
